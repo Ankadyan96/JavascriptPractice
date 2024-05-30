4c4c4fac4a8df8015bd913516fdc62e49382ed3d
Ankit Kadyan <107298686+Ankadyan96@users.noreply.github.com>
2024-05-30 07:17:58 +0000

    conversion of Datatype

--- 01_Basics/Test.js
+++ 01_Basics/Test.js
@@ -1,1 +1,39 @@
-console.log("Ankit Kadyan")
+// console.log("Ankit Kadyan")
+
+let score = "33"
+let scoreAbs = "33abs"
+let scores = false
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+let valueInNumberAbs = Number(scoreAbs);
+let valueInNumbers = Number(scores);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumberAbs);
+// console.log(valueInNumbers);
+
+// "33" => 33
+// "33abs" => NaN
+// undefined => NaN
+// null => 0
+// true => 1, false => 0.
+
+
+let isLoggedIn = "Ankit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "Ankit" => true
+
+let number = 33
+
+let stringNumber = String(number)
+
+console.log(stringNumber)
+console.log(typeof stringNumber);
