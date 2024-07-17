e820cd6a46637120b8b64cf5efad1e8a6208df8c
Ankit Kadyan <107298686+Ankadyan96@users.noreply.github.com>
2024-07-17 06:50:32 +0000

    Data type summary

---
+++ 01_Basics/Operations.js
@@ -0,0 +1,52 @@
+let value = 5;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); (power)
+// console.log(2/3);
+// console.log(2%3);
+
+/*************Data Type***************/
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1); 
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+
+/********Data Type of JS summary ********/
+
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// reference (Non-primitive)
+
+// Array, Objects, Functions
+
+const array = ["Car", "Bus", "Marvel", "DC"];
+
+let myObj = {
+    name: "Ankit",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
